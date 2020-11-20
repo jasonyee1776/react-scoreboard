eff07e2e2ffb885ed8ae61f3b3292f1f0b343da8
Jason Yee <jasonyee1776@gmail.com>
2020-11-20 01:49:25 -0800

    added Stopwatch start/stop and reset button

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,16 +1,65 @@
 import React, { Component } from 'react';
 
-// Updates
-
 class Stopwatch extends Component {
 
+    state = {
+        isRunning: false,
+        elapsedTime: 0,
+        previousTime: 0
+    }
+
+    handleStopwatch = () => {
+        this.setState( prevState => ({ 
+            isRunning: !prevState.isRunning 
+        }))
+        if (!this.state.isRunning) {
+            this.setState({ previousTime: Date.now() })
+        }
+
+    }
+
+    /*
+        React Lifecycle Methods (alos called Lifecycle Hooks)
+            - Built-in methods that get called at each point in the life cycel
+            - Act as Hooks that run code at key times in components life cycle
+            - Give us the abilyt to control what ahppens when component mounts. updates, unmounts
+    */
+
+    // componentDidMount() gets called by React wehn component gets inserted our mounted onto DOM
+    componentDidMount() {
+        this.intervalID = setInterval(() => this.tick(), 100)
+    }
+
+    // Anytime componentDidMount() is invoked it's important to also handle what should happen if the component were to unmount
+    // Important to handle unmounted components as it could cause memory leaks
+
+    componentWillUnmount() {
+        clearInterval(this.intervalID);
+    }
+
+    tick = () => {
+        if (this.state.isRunning) {
+            const now = Date.now();
+            this.setState( prevState => ({
+                previousTime: now,
+                elapsedTime: prevState.elapsedTime + (now - this.state.previousTime) 
+            }))
+        }
+    }
+
+    handleReset= () => {
+        this.setState({ elapsedTime: 0 });
+    }
+    
+
     render() {
+        const seconds = Math.floor(this.state.elapsedTime/1000);
         return(
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
-                <span className="stopwatch-time">0</span>
-                <button>Start</button>
-                <button>Reset</button>
+                <span className="stopwatch-time">{ seconds }</span>
+                <button onClick={this.handleStopwatch}>{this.state.isRunning ? "Stop" : "Start"}</button>
+                <button onClick={this.handleReset}>Reset</button>
             </div>
         )
     };
